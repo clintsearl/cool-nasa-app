7bc9b8aedd35e83f40d95190ddb2981c2fa7a2de
clintsearl <clintsearl@gmail.com>
2019-03-29 13:51:54 -0600

    deploying to heroku

---
+++ build/precache-manifest.149725cbc59404ce3ad8ee32f6d9d43c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9840f0a495cad108892e",
+    "url": "/static/css/main.ae944f05.chunk.css"
+  },
+  {
+    "revision": "9840f0a495cad108892e",
+    "url": "/static/js/main.04147824.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e968115e0ba438925424",
+    "url": "/static/css/2.c7c3f3f4.chunk.css"
+  },
+  {
+    "revision": "e968115e0ba438925424",
+    "url": "/static/js/2.a13a5064.chunk.js"
+  },
+  {
+    "revision": "b69a2b23559e31958e33b89be0efc899",
+    "url": "/index.html"
+  }
+];
