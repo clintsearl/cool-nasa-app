e819be54a709f0995b796028265eb6aabfa71d6f
clintsearl <clintsearl@gmail.com>
2019-09-26 17:29:59 -0600

    Changed up the styling, updated npm packages.

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -6,23 +6,23 @@ import "react-datepicker/dist/react-datepicker.css"
 
 const DateInput = props => (
       <div>
+        <h1>Cool Nasa App</h1>
+        <h6>Check out Nasa's featured picture of the day</h6>
         <p class ='label'>Select a date</p>
           <DatePicker 
           selected = {props.date}
           onChange = {props.changeDate}
           />
-
+          <p></p>
         {/* So this is the old boring input that still works */}
-
     {/* <form onSubmit ={props.changeDate}>
          Enter A date (YYYY-MM-DD):
          <input/>
          <input type= "submit"/>
 
      </form> */}
-
-
       </div>
 )
+// I opted for react date picker instead that was more simple and fit what I needed for this application.
 // moment.format('YYYY-MM-DD')
 export default DateInput;
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,7 +6,7 @@ import React from 'react';
 const Photo = (props)=>(
       <div>
           <h2>{props.photo.title}</h2>
-          <img src ={props.photo.url} alt ={props.photo.title} style={{resizeMode:"contain", maxWidth:"100%", resizeMethod:"scale"}}/>
+          <img src ={props.photo.url} alt ={props.photo.title} style={{resizeMode:"contain", maxWidth:"94%", resizeMethod:"scale", border:"10px groove" }}/>
        <p>{props.photo.explanation}</p>
       </div>
 
